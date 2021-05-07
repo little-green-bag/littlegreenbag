0f172a65989045c3d644b499abcfcb9cf4dfa38c
Cillian O Murchu <cillian.murchu@gmail.com>
2021-05-07 14:19:35 +0200

    removed unnecessary sidebar

--- src/app/responsive-sidenav/responsive-sidenav.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ResponsiveSidenavDirective } from './responsive-sidenav.directive';
-
-describe('ResponsiveSidenavDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ResponsiveSidenavDirective();
-    expect(directive).toBeTruthy();
-  });
-});
